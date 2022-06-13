a6f7b818948a5d4bf8ea71572fcdf66e5e308e65
Lorenzo Limoli <l.limoli@reply.it>
2022-06-13 12:58:34 +0200

    Initial Commit

---
+++ vip_architecture_example/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  vip_architecture_example
+//
+//  Created by Lorenzo Limoli on 13/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
